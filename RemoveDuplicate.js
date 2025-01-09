4bd2be6bc487f40f4128ae908cb6e6537a304ae3
rajeshaanand <rajesh.aanand@gmail.com>
2025-01-09 16:51:41 -0500

    Update RemoveDuplicate.js

--- RemoveDuplicate.js
+++ RemoveDuplicate.js
@@ -3,4 +3,4 @@ var removeDuplicate = function (nums) {
   let withoutDuplicate = new Set(nums);
   return [...withoutDuplicate];
 };
-console.log(removeDuplicate([2, 3, 4, 6, 7, 8, 2]));
+console.log(removeDuplicate([2, 3, 4, 6, 7, 8, 2, 8]));
